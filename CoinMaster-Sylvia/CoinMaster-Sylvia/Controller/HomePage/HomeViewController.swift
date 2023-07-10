984e44905bc1ecb5ce04592916706ac11d7f92d0
“Sin <sinlin0n0@gmail.com>
2023-07-10 15:37:57 +0800

    Fix:Account balabce.

--- CoinMaster-Sylvia/CoinMaster-Sylvia/Controller/HomePage/HomeViewController.swift
+++ CoinMaster-Sylvia/CoinMaster-Sylvia/Controller/HomePage/HomeViewController.swift
@@ -19,6 +19,7 @@ class HomeViewController: UIViewController {
     var usdPairsStats: [String: (Double, Double)] = [:]
     var formattedBalance: String = ""
     var twdExchangeRate: Double?
+    var totalBalanceInTWD: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,32 +34,25 @@ class HomeViewController: UIViewController {
         super.viewWillAppear(animated)
         tabBarController?.tabBar.isHidden = false
         self.getApiData()
-       
+        
     }
-
+    
     func getApiData(completion: (([CurrencyPair]) -> Void)? = nil) {
         var balanceExchange: Double!
         let semaphore = DispatchSemaphore(value: 0)
+        self.totalBalanceInTWD = 0
         CoinbaseService.shared.getApiResponse(api: CoinbaseApi.accounts,
                                               authRequired: true,
                                               requestPath: RequestPath.accounts,
                                               httpMethod: HttpMethod.get) { [weak self] (accounts: [Account]) in
             for account in accounts {
-                if account.currency == "USD" {
-                    let balance = account.balance
-                    balanceExchange = Double(balance) ?? 0
+                if let balance = Double(account.balance) {
+                    let rate = account.currency
+                    balance.convertToTWD(rate: rate) { convertedValue in
+                        self?.totalBalanceInTWD += convertedValue
+                    }
                 }
             }
-            let rate = "USD"
-            CoinbaseService.shared.getApiSingleResponse(api: CoinbaseApi.exchangeRate, param: rate, authRequired: false)  { [weak self] (exchangeRate: ExchangeRateResponse) in
-                if let twdExchangeRate = exchangeRate.data.rates["TWD"] {
-                    self?.twdExchangeRate = (Double(twdExchangeRate) ?? 0)
-                    let balanceTWD = balanceExchange * (Double(twdExchangeRate) ?? 0)
-                    let formattedBalance = NumberFormatter.formattedNumber(balanceTWD)
-                    self?.formattedBalance = formattedBalance
-                }
-                
-                // fetch usdPairsAPI
                 CoinbaseService.shared.getApiResponse(api: CoinbaseApi.products,
                                                       authRequired: false) { [weak self] (products: [CurrencyPair]) in
                     
@@ -70,26 +64,26 @@ class HomeViewController: UIViewController {
                     for pair in self!.usdPairs {
                         group.enter()
                         
-                            CoinbaseService.shared.getApiSingleResponse(api: CoinbaseApi.products,
-                                                                        param: "/\(pair.id)/stats",
-                                                                        authRequired: false) { [weak self] (products: ProductsStats) in
-                                let open = Double(products.open) ?? 0
-                                let last = Double(products.last) ?? 0
-                                let trend = (last - open) / last * 100
-
-                                let low = Double(products.low) ?? 0
-                                let high = Double(products.high) ?? 0
-                                let average = (low + high) / 2
-                                
-                                self?.usdPairsStats.updateValue((average, trend), forKey: pair.id)
-                                group.leave()
+                        CoinbaseService.shared.getApiSingleResponse(api: CoinbaseApi.products,
+                                                                    param: "/\(pair.id)/stats",
+                                                                    authRequired: false) { [weak self] (products: ProductsStats) in
+                            let open = Double(products.open) ?? 0
+                            let last = Double(products.last) ?? 0
+                            let trend = (last - open) / last * 100
+                            
+                            let low = Double(products.low) ?? 0
+                            let high = Double(products.high) ?? 0
+                            let average = (low + high) / 2
+                            
+                            self?.usdPairsStats.updateValue((average, trend), forKey: pair.id)
+                            group.leave()
                         }
                     }
                     group.notify(queue: .main) {
                         self?.tableView?.reloadData()
                     }
                 }
-            }
+            
             semaphore.signal()
         }
         semaphore.wait()
@@ -99,7 +93,7 @@ class HomeViewController: UIViewController {
         self.tableView!.reloadData()
         self.tableView.mj_header?.endRefreshing()
     }
-
+    
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
@@ -113,7 +107,12 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
                 print("error")
                 return UITableViewCell()
             }
-            cell.balanceLabel.text = formattedBalance
+            if self.totalBalanceInTWD == 0 {
+                cell.balanceLabel.text = ""
+            } else {
+                cell.balanceLabel.text = NumberFormatter.formattedNumber(self.totalBalanceInTWD)
+            }
+            
             return cell
         } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell2", for: indexPath) as? HomeTableViewCell2 else {
